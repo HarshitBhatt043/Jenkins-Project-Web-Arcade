3675172c9ba9858e57b1ea466395c63b0ef53134
Harshit Bhatt <043.harshitbhatt@gmail.com>
2023-08-17 16:51:31 +0530

    Added HD Textures and Updated Gamepad script

--- launch.js
+++ launch.js
@@ -88,16 +88,6 @@
     return window.location.reload();
   };
 
-  $("s-credits").onclick = function () {
-    $("step-1").style.display = "none";
-    return ($("credits").style.display = "block");
-  };
-
-  $("credits").onclick = function () {
-    $("step-1").style.display = "block";
-    return ($("credits").style.display = "none");
-  };
-
   hasWebGL = function () {
     var canvas, gl;
     gl = null;
