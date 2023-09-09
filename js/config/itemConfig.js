f863735b1eb5971ad4aea237b50bf544de9d5213
Harshit Bhatt <043.harshitbhatt@gmail.com>
2023-09-09 13:41:54 +0530

    Reverted the code

--- js/config/itemConfig.js
+++ js/config/itemConfig.js
@@ -1,5 +1,1 @@
-itemConfig = {
-  0: { name: "Triple foam props", price: 100 },
-  1: { name: "Combo score extra +1", price: 200 },
-  2: { name: "Missed fruit protection three times", price: 300 },
-};
+itemConfig={0:{name:"Triple foam props",price:100},1:{name:"Combo score extra +1",price:200},2:{name:"Missed fruit protection three times",price:300}};
