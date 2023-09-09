0e1c8af32e1d08f1efafec88e64acb27eb4eb650
Harshit Bhatt <043.harshitbhatt@gmail.com>
2023-09-09 12:48:54 +0530

    Formatted the code

--- js/View/SettingPanel.js
+++ js/View/SettingPanel.js
@@ -1,1 +1,10 @@
-function SettingPanel(){SettingPanel.super(this),Laya.Tween,Laya.Ease,Laya.Event,this.OpenPanel=function(){},this.onUpdate=function(){},this.ClosePanel=function(){}}Laya.class(SettingPanel,"SettingPanel",SettingUI);
+function SettingPanel() {
+  SettingPanel.super(this),
+    Laya.Tween,
+    Laya.Ease,
+    Laya.Event,
+    (this.OpenPanel = function () {}),
+    (this.onUpdate = function () {}),
+    (this.ClosePanel = function () {});
+}
+Laya.class(SettingPanel, "SettingPanel", SettingUI);
