862a37f466dfc03783e1d2f120ba0018a9718b5a
Harshit Bhatt <043.harshitbhatt@gmail.com>
2023-09-17 00:49:13 +0530

    Added UI UX code

---
+++ popup.js
@@ -0,0 +1,92 @@
+const pacman = "popup_visited7";
+
+function startTypingAnimation() {
+  const title = document.getElementById("popup-title");
+  const message = document.getElementById("popup-message");
+  const titleText = "INSTRUCTIONS";
+  const messageText = [
+    "For desktop use W,A,S,D to move.",
+    "For mobile use swipe to move.",
+    "Do you want to see this announcement again?",
+  ];
+
+  const typingSpeed = 50;
+  let currentIndex = 0;
+
+  function typeNextLine() {
+    if (currentIndex < messageText.length) {
+      const line = document.createElement("div");
+      message.appendChild(line);
+      const lineText = messageText[currentIndex];
+      let lineIndex = 0;
+
+      function typeNextCharacter() {
+        if (lineIndex < lineText.length) {
+          line.textContent += lineText.charAt(lineIndex);
+          lineIndex++;
+          setTimeout(typeNextCharacter, typingSpeed);
+        } else {
+          currentIndex++;
+          setTimeout(typeNextLine, typingSpeed);
+        }
+      }
+
+      typeNextCharacter();
+    }
+  }
+
+  typeText(title, titleText, 0, typingSpeed);
+  setTimeout(typeNextLine, titleText.length * typingSpeed);
+}
+
+function typeText(element, text, index, speed) {
+  if (index < text.length) {
+    element.textContent += text.charAt(index);
+    index++;
+    setTimeout(function () {
+      typeText(element, text, index, speed);
+    }, speed);
+  }
+}
+
+function closePopup() {
+  const popup = document.getElementById("popup");
+  const popupContent = document.querySelector(".popup-content");
+  popupContent.style.animation =
+    "popOut 0.5s ease forwards, fadeOut 0.5s ease forwards";
+  setTimeout(function () {
+    popup.style.display = "none";
+  }, 500);
+}
+
+document.getElementById("yesButton").addEventListener("click", function () {
+  localStorage.setItem(pacman, "yes");
+  closePopup();
+});
+
+document.getElementById("noButton").addEventListener("click", function () {
+  localStorage.setItem(pacman, "no");
+  closePopup();
+});
+
+document.getElementById("popup").addEventListener("click", function (event) {
+  if (event.target === this) {
+    closePopup();
+  }
+});
+
+function checkLocalStorage() {
+  const visited = localStorage.getItem(pacman);
+  if (visited !== "no") {
+    const popup = document.getElementById("popup");
+    const popupContent = document.querySelector(".popup-content");
+    popup.style.display = "block";
+    popupContent.style.animation =
+      "popIn 0.5s ease forwards, fadeIn 0.5s ease forwards";
+    startTypingAnimation();
+  }
+}
+
+window.addEventListener("load", function () {
+  checkLocalStorage();
+});
