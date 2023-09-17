76eb131459699c8dc7be88c78f59cb7d7d6a26ee
Harshit Bhatt <043.harshitbhatt@gmail.com>
2023-09-17 21:34:18 +0530

    Added UI UX code

---
+++ popup.js
@@ -0,0 +1,113 @@
+const car = "popup_visited11";
+let popupClosed = false;
+
+function LoaderScript() {
+  var gameInstance = UnityLoader.instantiate(
+    "gameContainer",
+    "Build/cars-simulator-v2.json",
+    {
+      onProgress: UnityProgress,
+      Module: {
+        onRuntimeInitialized: function () {
+          UnityProgress(gameInstance, "complete");
+        },
+      },
+    }
+  );
+}
+
+function startTypingAnimation() {
+  const title = document.getElementById("popup-title");
+  const message = document.getElementById("popup-message");
+  const titleText = "INSTRUCTIONS";
+  const messageText = [
+    "Make sure you give this game time to load.",
+    "It may stuck while loading progress.",
+    "If it takes more than 5 minutes, refresh the page!",
+    "Do you want to see this announcement again?",
+  ];
+
+  const typingSpeed = 50;
+  let currentIndex = 0;
+
+  function typeNextLine() {
+    if (currentIndex < messageText.length) {
+      const line = document.createElement("div");
+      message.appendChild(line);
+      const lineText = messageText[currentIndex];
+      let lineIndex = 0;
+
+      function typeNextCharacter() {
+        if (lineIndex < lineText.length) {
+          line.textContent += lineText.charAt(lineIndex);
+          lineIndex++;
+          setTimeout(typeNextCharacter, typingSpeed);
+        } else {
+          currentIndex++;
+          setTimeout(typeNextLine, typingSpeed);
+        }
+      }
+
+      typeNextCharacter();
+    }
+  }
+
+  typeText(title, titleText, 0, typingSpeed);
+  setTimeout(typeNextLine, titleText.length * typingSpeed);
+}
+
+function typeText(element, text, index, speed) {
+  if (index < text.length) {
+    element.textContent += text.charAt(index);
+    index++;
+    setTimeout(function () {
+      typeText(element, text, index, speed);
+    }, speed);
+  }
+}
+
+function closePopup() {
+  const popup = document.getElementById("popup");
+  const popupContent = document.querySelector(".popup-content");
+  popupContent.style.animation =
+    "popOut 0.5s ease forwards, fadeOut 0.5s ease forwards";
+  setTimeout(function () {
+    popup.style.display = "none";
+    popupClosed = true;
+    LoaderScript();
+  }, 500);
+}
+
+document.getElementById("yesButton").addEventListener("click", function () {
+  localStorage.setItem(car, "yes");
+  closePopup();
+});
+
+document.getElementById("noButton").addEventListener("click", function () {
+  localStorage.setItem(car, "no");
+  closePopup();
+});
+
+document.getElementById("popup").addEventListener("click", function (event) {
+  if (event.target === this) {
+    closePopup();
+  }
+});
+
+function checkLocalStorage() {
+  const visited = localStorage.getItem(car);
+  if (visited !== "no") {
+    const popup = document.getElementById("popup");
+    const popupContent = document.querySelector(".popup-content");
+    popup.style.display = "block";
+    popupContent.style.animation =
+      "popIn 0.5s ease forwards, fadeIn 0.5s ease forwards";
+    startTypingAnimation();
+  } else {
+    LoaderScript();
+  }
+}
+
+window.addEventListener("load", function () {
+  checkLocalStorage();
+});
