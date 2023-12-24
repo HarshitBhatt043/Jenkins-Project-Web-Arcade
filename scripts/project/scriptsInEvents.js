952c3397cd5857eb0833082acc6172b5a1754f99
Harshit Bhatt <043.harshitbhatt@gmail.com>
2023-12-24 17:45:54 +0530

    Formatted and Updated code

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -1,14 +1,7 @@
-
-
-
 const scriptsInEvents = {
-
-	async Intro_Event1_Act1(runtime, localVars)
-	{
-		runtime.globalVars.access_token = getWebsite_URL();
-	}
-
+  async Intro_Event1_Act1(runtime, localVars) {
+    runtime.globalVars.access_token = getWebsite_URL();
+  },
 };
 
 self.C3.ScriptsInEvents = scriptsInEvents;
-
