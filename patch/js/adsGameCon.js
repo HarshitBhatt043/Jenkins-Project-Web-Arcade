a67aed1582bd012d2c218693908f08ded2899afa
Harshit Bhatt <043.harshitbhatt@gmail.com>
2023-12-31 12:44:07 +0530

    Formatted and Updated code

--- patch/js/adsGameCon.js
+++ patch/js/adsGameCon.js
@@ -1,29 +1,28 @@
-consoleLog= console.log;
+consoleLog = console.log;
 
-adsGameCon= function() {
-  this.recordOpen= function() {
+adsGameCon = function () {
+  this.recordOpen = function () {
     consoleLog("--fx--adsGameCon--recordOpen--");
     return true;
-  }
+  };
 
-  this.showAd= function(options) {
+  this.showAd = function (options) {
     consoleLog("--fx--adsGameCon--showAd--", arguments);
     options.beforeShowAd();
     setTimeout(options.afterShowAd, 1000);
     return true;
-  }
+  };
 
-  this.showAdOfEvent= function(options) {
+  this.showAdOfEvent = function (options) {
     consoleLog("--fx--adsGameCon--showAdOfEvent--", arguments);
     options.beforeShowAd();
     options.afterShowAd();
     // options.onRewardDismissed();
-  }
+  };
 
-  this.moreGame= function() {
+  this.moreGame = function () {
     consoleLog("--fx--adsGameCon--moreGame--", arguments);
-  }
-  
-}
+  };
+};
 
-adsGameCon= new adsGameCon();
+adsGameCon = new adsGameCon();
