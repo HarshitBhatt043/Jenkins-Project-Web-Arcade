fd4322478939fa45d0ca3dbf893e90836b00a121
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-01-06 16:43:06 +0530

    test

---
+++ scripts/asset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo 'Checking path and downloading asset'
+
+directory_path="$CIRCLE_WORKING_DIRECTORY/$ASSETPATH"
+filesCount=$(ls -1 "${directory_path}" | wc -l | tr -d '[:space:]')
+
+if [ "$filesCount" -gt 0 ]; then
+    echo "Asset already available."
+else
+    mkdir -p "${directory_path}"
+    wget -q "$ASSET" -P "${directory_path}"
+    mv "${directory_path}"/* "$CIRCLE_WORKING_DIRECTORY/$ASSETNAME"
+
+    downloadedfilesCount=$(ls -1 "$CIRCLE_WORKING_DIRECTORY/$ASSETNAME" | wc -l | tr -d '[:space:]')
+
+    if [ "$downloadedfilesCount" -gt 0 ]; then
+        echo "Asset download successful."
+    else
+        echo "Pipeline aborted due to failure: Link changed or Expired"
+        exit 1
+    fi
+fi
