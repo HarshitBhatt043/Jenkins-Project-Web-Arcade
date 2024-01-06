2c2057428214facaa749e6cb03a0cdc9c18d879b
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-01-06 10:35:08 +0530

    test

--- scripts/qc.sh
+++ scripts/qc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-projectKey=$(grep '^sonar.projectKey=' "<< pipeline.parameters.sonarPropertiesFile >>" | cut -d'=' -f2 | tr -d '[:space:]')
+projectKey=$(grep '^sonar.projectKey=' "$CIRCLE_WORKING_DIRECTORY/sonar-project.properties" | cut -d'=' -f2 | tr -d '[:space:]')
 value=$(curl -s "https://sonarcloud.io/api/measures/component?component=${projectKey}&branch=circleci-project-setup&metricKeys=alert_status" | grep -o '"value":"[^"]*"' | cut -d':' -f2 | tr -d '"')
 
 if [ "$value" == "OK" ]; then
