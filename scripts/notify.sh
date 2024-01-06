65d05f48e834daf6e3deeadfb7a78305a2c6b4b1
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-01-06 14:26:44 +0530

    gg

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -4,7 +4,6 @@ GIT_COMMIT=$(git rev-parse HEAD)
 GIT_MESSAGE=$(git log -n 1 --format=%s "${GIT_COMMIT}")
 GIT_AUTHOR=$(git log -n 1 --format=%ae "${GIT_COMMIT}")
 GIT_COMMIT_SHORT=$(git rev-parse --short "${GIT_COMMIT}")
-GIT_INFO="Branch(Version): main\\\nLast Message: ${GIT_MESSAGE}\\\nAuthor: ${GIT_AUTHOR}\\\nCommit: ${GIT_COMMIT_SHORT}"
 
 projectKey=$(grep '^sonar.projectKey=' "./sonar-project.properties" | cut -d'=' -f2 | tr -d '[:space:]')
 metricKeys='bugs,vulnerabilities,security_hotspots,code_smells,duplicated_lines_density,ncloc,cognitive_complexity,critical_violations,major_violations,sqale_index,alert_status'
@@ -13,7 +12,8 @@ metricsMap=$(echo "$sonarQubeResult" | jq -r '.')
 
 text_break='------------------------------------------------------------------------'
 info_break='------------------------------------'
-notificationMessage=$(cat <<EOF
+notificationMessage=$(
+    cat <<EOF
 ${text_break}
 *Build Information:*
 Job: $CIRCLE_JOB
@@ -21,7 +21,10 @@ Build Number: $CIRCLE_BUILD_NUM
 Build URL: [$CIRCLE_JOB]($CIRCLE_BUILD_URL)
 ${info_break}
 *SCM Information:*
-${GIT_INFO}
+Branch(Version): main
+Last Message: ${GIT_MESSAGE}
+Author: ${GIT_AUTHOR}
+Commit: ${GIT_COMMIT_SHORT}
 ${info_break}
 *SonarCloud Information:*
 Quality Gate: $(echo "${metricsMap}" | jq -r '.component.measures[] | select(.metric == "alert_status") | .value' || echo 'N/A')
@@ -44,5 +47,3 @@ curl -sL --request POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
     --form text="${notificationMessage}" \
     --form chat_id="$CHAT_ID" \
     --form parse_mode='Markdown'
-
-
