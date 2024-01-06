c3d2e458c503090ec62b41d642aaa0ef5463502e
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-01-06 10:29:01 +0530

    test

--- scripts/qc.sh
+++ scripts/qc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-value=$(curl -s 'https://sonarcloud.io/api/measures/component?component=HarshitBhatt043_Jenkins-Project-Web-Arcade&branch=circleci-project-setup&metricKeys=alert_status' | grep -o '"value":"[^"]*"' | cut -d':' -f2 | tr -d '"')
+
+projectKey=$(grep '^sonar.projectKey=' "<< pipeline.parameters.sonarPropertiesFile >>" | cut -d'=' -f2 | tr -d '[:space:]')
+value=$(curl -s "https://sonarcloud.io/api/measures/component?component=${projectKey}&branch=circleci-project-setup&metricKeys=alert_status" | grep -o '"value":"[^"]*"' | cut -d':' -f2 | tr -d '"')
 
 if [ "$value" == "OK" ]; then
     echo "Quality gate passed"
