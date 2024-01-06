9396099e9d85b4ce954bfa4fc7ffc8bd2d5eb661
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-01-06 14:21:38 +0530

    test

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -4,7 +4,7 @@ GIT_COMMIT=$(git rev-parse HEAD)
 GIT_MESSAGE=$(git log -n 1 --format=%s "${GIT_COMMIT}")
 GIT_AUTHOR=$(git log -n 1 --format=%ae "${GIT_COMMIT}")
 GIT_COMMIT_SHORT=$(git rev-parse --short "${GIT_COMMIT}")
-GIT_INFO="Branch(Version): main\\nLast Message: ${GIT_MESSAGE}\\nAuthor: ${GIT_AUTHOR}\\nCommit: ${GIT_COMMIT_SHORT}"
+GIT_INFO="Branch(Version): main\\\nLast Message: ${GIT_MESSAGE}\\\nAuthor: ${GIT_AUTHOR}\\\nCommit: ${GIT_COMMIT_SHORT}"
 
 projectKey=$(grep '^sonar.projectKey=' "./sonar-project.properties" | cut -d'=' -f2 | tr -d '[:space:]')
 metricKeys='bugs,vulnerabilities,security_hotspots,code_smells,duplicated_lines_density,ncloc,cognitive_complexity,critical_violations,major_violations,sqale_index,alert_status'
