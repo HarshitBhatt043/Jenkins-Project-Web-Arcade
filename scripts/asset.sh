6b06931b4c20430c5ee420d4d96e4d89210be1f8
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-01-06 17:00:16 +0530

    test

--- scripts/asset.sh
+++ scripts/asset.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 set -e
 
-echo 'Checking path and downloading asset'
-
 directory_path="$CIRCLE_WORKING_DIRECTORY/$ASSETPATH"
-filesCount=$(ls -1 "${directory_path}" | wc -l | tr -d '[:space:]')
 
-mkdir -p "${directory_path}"
+if [ -d "$directory_path" ]; then
+    filesCount=$(ls -1 "$directory_path" | wc -l | tr -d '[:space:]')
 
-if [ "$filesCount" -gt 0 ]; then
-    echo "Asset already available."
+    if [ "$filesCount" -gt 0 ]; then
+        echo "Asset already available."
+    else
+        mkdir -p "$directory_path"
+        wget -q "$ASSET" -P "$directory_path"
+        mv "$directory_path"/* "$CIRCLE_WORKING_DIRECTORY/$ASSETNAME"
+
+        downloadedFilesCount=$(ls -1 "$CIRCLE_WORKING_DIRECTORY/$ASSETNAME" | wc -l | tr -d '[:space:]')
+
+        if [ "$downloadedFilesCount" -gt 0 ]; then
+            echo "Asset download successful."
+        else
+            echo "Pipeline aborted due to failure: Link changed or Expired"
+            exit 1
+        fi
+    fi
 else
-    mkdir -p "${directory_path}"
-    wget -q "$ASSET" -P "${directory_path}"
-    mv "${directory_path}"/* "$CIRCLE_WORKING_DIRECTORY/$ASSETNAME"
+    mkdir -p "$directory_path"
+    wget -q "$ASSET" -P "$directory_path"
+    mv "$directory_path"/* "$CIRCLE_WORKING_DIRECTORY/$ASSETNAME"
 
-    downloadedfilesCount=$(ls -1 "$CIRCLE_WORKING_DIRECTORY/$ASSETNAME" | wc -l | tr -d '[:space:]')
+    downloadedFilesCount=$(ls -1 "$CIRCLE_WORKING_DIRECTORY/$ASSETNAME" | wc -l | tr -d '[:space:]')
 
-    if [ "$downloadedfilesCount" -gt 0 ]; then
+    if [ "$downloadedFilesCount" -gt 0 ]; then
         echo "Asset download successful."
     else
         echo "Pipeline aborted due to failure: Link changed or Expired"
