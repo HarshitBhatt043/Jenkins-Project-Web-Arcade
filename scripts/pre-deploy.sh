4eac4b8be9289a7fe9bcf74415b4b05232aee722
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-01-08 00:09:44 +0530

    test

--- scripts/pre-deploy.sh
+++ scripts/pre-deploy.sh
@@ -4,11 +4,40 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
+install_docker() {
+    conflicting_packages=("docker.io" "docker-doc" "docker-compose" "docker-compose-v2" "podman-docker" "containerd" "runc")
+    for pkg in "${conflicting_packages[@]}"; do
+        sudo apt-get remove -y "$pkg"
+    done
+
+    sudo apt update && sudo apt upgrade -y
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    sudo apt-get update
+    echo "Installing Docker..."
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin ||
+        {
+            echo "Error installing Docker. Exiting."
+            exit 1
+        }
+
+}
+
 install_netdata() {
     echo "Installing Netdata..."
     curl -sSL https://my-netdata.io/kickstart.sh >/tmp/netdata-kickstart.sh &&
         sh /tmp/netdata-kickstart.sh --stable-channel --disable-telemetry --claim-token CLAIMTOKEN --claim-rooms ROOMID --claim-url https://app.netdata.cloud ||
-        { echo "Error installing Netdata. Exiting."; exit 1; }
+        {
+            echo "Error installing Netdata. Exiting."
+            exit 1
+        }
 }
 
 install_cloudflared() {
@@ -16,8 +45,23 @@ install_cloudflared() {
     curl -sSL --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb &&
         sudo dpkg -i cloudflared.deb &&
         sudo cloudflared service install CLOUDTOKEN ||
-        { echo "Error installing Cloudflared. Exiting."; exit 1; }
+        {
+            echo "Error installing Cloudflared. Exiting."
+            exit 1
+        }
 }
+
+if ! command_exists "docker"; then
+    if [ "$EUID" -eq 0 ]; then
+        echo "User is root. Installing Docker..."
+        install_docker
+    elif sudo -n true 2>/dev/null; then
+        echo "User has passwordless sudo access. Installing Docker..."
+        install_docker
+    else
+        error "Docker install needs passwordless sudo access. Aborting."
+    fi
+fi
 
 if ! command_exists "netdata"; then
     install_netdata
@@ -25,6 +69,11 @@ fi
 
 if ! command_exists "cloudflared"; then
     install_cloudflared
+fi
+
+if ! pgrep -x "dockerd" >/dev/null; then
+    echo "Docker is not running. Exiting."
+    exit 1
 fi
 
 if ! pgrep -x "netdata" >/dev/null; then
@@ -37,4 +86,4 @@ if ! pgrep -x "cloudflared" >/dev/null; then
     exit 1
 fi
 
-echo "Both Netdata and Cloudflared are installed and running."
+echo "Docker,Netdata and Cloudflared are installed and running."
