a5e09b88d6a272ebf6175667a516f47f9b573da3
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-01-12 14:53:37 +0530

    Fixed paths and remaning assets

--- js/scr.js
+++ js/scr.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
     (e.className = "imgb"),
     (e.style = "position:fixed;top:10%;left:-100px;z-index:10"),
     (e.innerHTML =
-      '<a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley/" title="More of best Classroom 6x Unblocked Games"><img src="https://lh4.googleusercontent.com/lUEWrXMVEr4AdjKISyJahDRJ61bwfvHdpeYm86Djn5U8oCm9dI60NGXSBqad9HUvzTXgqlkosA_hWV-VuXPjzrkGvh3_kNSgYk8ySWzXnDpbBCBiooyBbU8oBy3YBZMDkW8RcRVmDuC0raoeqZBm8kBlqs6c5mdfkJeN2aE68lXS_lcOZ5_F7lIuM6qLVg" width="100" height="30" style="cursor:pointer;" alt="More Unblocked Games 6x"></a>'),
+      '<a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley/" title="More">alt="More"></a>'),
     document.body.appendChild(e);
   var t = document.getElementById("button"),
     n = 0,
