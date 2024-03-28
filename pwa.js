a10bd7695f7643e136add8cefcf84f54e9c3cf2d
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 12:21:41 +0530

    Compressed code and added PWA support

---
+++ pwa.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){document.documentElement,window.matchMedia("(max-width: 600px)").matches;const e="./2048.js";navigator.serviceWorker.register(e).then((()=>{})).catch((e=>{}))}
