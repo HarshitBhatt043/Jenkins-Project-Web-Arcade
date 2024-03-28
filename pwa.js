900b18b15ab4c2fd192464530c4112eb09142200
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 13:06:44 +0530

    Compressed code and added PWA support

---
+++ pwa.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){document.documentElement,window.matchMedia("(max-width: 600px)").matches;const e="./Rooftop_Snipers_2.js";navigator.serviceWorker.register(e).then((()=>{})).catch((e=>{}))}
