22c3b8e8e81fa22a4cc6352a75971d9da879a2cc
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 12:46:26 +0530

    Compressed code and added PWA support

---
+++ pwa.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){document.documentElement,window.matchMedia("(max-width: 600px)").matches;const e="./Evil_Glitch.js";navigator.serviceWorker.register(e).then((()=>{})).catch((e=>{}))}
