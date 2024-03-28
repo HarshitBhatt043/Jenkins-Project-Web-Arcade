5fafc17b18fd6dec8f529a658fc9746dcc742758
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 12:45:49 +0530

    Compressed code and added PWA support

---
+++ assets.js
@@ -0,0 +1,1 @@
+["./android-chrome-192x192.png","./android-chrome-512x512.png","./apple-touch-icon.png","./assets.js","./Build/itchio.data","./Build/itchio.framework.js","./Build/itchio.loader.js","./Build/itchio.wasm","./favicon-16x16.png","./favicon-32x32.png","./favicon.ico","./index.html","./site.webmanifest","./toast.css","./toast.js"]
