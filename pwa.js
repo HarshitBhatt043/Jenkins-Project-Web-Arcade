b5f6cb24ec495e8ef38aca73bf26b28a9e4e23ef
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 12:30:57 +0530

    Compressed code and added PWA support

---
+++ pwa.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){document.documentElement,window.matchMedia("(max-width: 600px)").matches;const e="./Color_Switch.js";navigator.serviceWorker.register(e).then((()=>{})).catch((e=>{}))}
