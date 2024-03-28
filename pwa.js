e258d90ce5449ee9cdf21024e9c90b61409570bb
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 13:19:03 +0530

    Compressed code and added PWA support

---
+++ pwa.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){document.documentElement,window.matchMedia("(max-width: 600px)").matches;const e="./SnowRider_3D.js";navigator.serviceWorker.register(e).then((()=>{})).catch((e=>{}))}
