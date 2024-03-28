afc5d8ab32d1146707248fcde2caa15ee1340a76
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 13:20:04 +0530

    Compressed code and added PWA support

---
+++ assets.js
@@ -0,0 +1,1 @@
+["./android-chrome-192x192.png","./android-chrome-512x512.png","./apple-touch-icon.png","./assets.js","./favicon-16x16.png","./favicon-32x32.png","./favicon.ico","./featured-stack.jpg","./index.html","./main.js","./site.webmanifest","./stack.png","./three.min.js","./toast.css","./toast.js","./TweenMax.min.js"]
