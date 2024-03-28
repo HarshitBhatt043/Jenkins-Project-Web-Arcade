5dadf3990247528233cfa3798843f44e7b1f3ba7
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 13:04:03 +0530

    Compressed code and added PWA support

--- color.js
+++ color.js
@@ -1,16 +1,1 @@
-function Color(r, g, b) {
-  if (r instanceof Array) {
-    return Color.apply(this, r);
-  }
-  this.r = r;
-  this.g = g;
-  this.b = b;
-}
-
-Color.prototype.rgb = function () {
-  return "rgb(" + this.r + "," + this.g + "," + this.b + ")";
-};
-
-Color.prototype.equals = function (col) {
-  return col.r === this.r && col.g === this.g && col.b === this.b;
-};
+function Color(t,r,o){if(t instanceof Array)return Color.apply(this,t);this.r=t,this.g=r,this.b=o}Color.prototype.rgb=function(){return"rgb("+this.r+","+this.g+","+this.b+")"},Color.prototype.equals=function(t){return t.r===this.r&&t.g===this.g&&t.b===this.b};
