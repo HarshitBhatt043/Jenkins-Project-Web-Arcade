636f7f484c6a2dbe12c12e9a7bc0428ef4176cab
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 12:49:28 +0530

    Compressed code and added PWA support

---
+++ pwa.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){document.documentElement,window.matchMedia("(max-width: 600px)").matches;const e="./Game_Inside.js";navigator.serviceWorker.register(e).then((()=>{})).catch((e=>{}))}
