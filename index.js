7c47d43f7d8b8e2a29262167f08edc9c09336029
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 13:26:17 +0530

    Compressed code and added PWA support

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const express = require("express");
-const path = require("path");
-const opn = require("opn");
-
-const server = express();
-const host = "http://localhost:8082";
-server.use("/assets", express.static(path.resolve(__dirname, "./assets")));
-server.use("/dist", express.static(path.resolve(__dirname, "./dist")));
-
-server.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./index.html"));
-});
-
-server.listen(8082, () => {
-  console.log(`server started at ${host}`);
-  opn(host);
-});
+const express=require("express"),path=require("path"),opn=require("opn"),server=express(),host="http://localhost:8082";server.use("/assets",express.static(path.resolve(__dirname,"./assets"))),server.use("/dist",express.static(path.resolve(__dirname,"./dist"))),server.get("*",((e,s)=>{s.sendFile(path.resolve(__dirname,"./index.html"))})),server.listen(8082,(()=>{opn(host)}));
