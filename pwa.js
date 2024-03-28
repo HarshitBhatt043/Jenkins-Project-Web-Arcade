82157d73456b59917573698226a4ab17fcacc3a9
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 12:30:17 +0530

    Compressed code and added PWA support

---
+++ pwa.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){document.documentElement,window.matchMedia("(max-width: 600px)").matches;const e="./Cluster_Rush.js";navigator.serviceWorker.register(e).then((()=>{})).catch((e=>{}))}
