40a7a92728a539a989961dea7ff5f808c2b5cb67
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 12:27:10 +0530

    Compressed code and added PWA support

--- js/unity-mod.js
+++ js/unity-mod.js
@@ -1,13 +1,1 @@
-function UnityOpen(url) {
-  url = "#" + url;
-  console.log("--fx--UnityOpen--", url);
-  return url;
-}
-
-function UnityUrlFix(url) {
-  console.log("--fx--UnityUrlFix--", url);
-  if (url.indexOf("unity3d.com") > 0 || url.indexOf("appspot.com") > 0) {
-    url = "json/null.json?" + url;
-  }
-  return url;
-}
+function UnityOpen(n){return n="#"+n}function UnityUrlFix(n){return(n.indexOf("unity3d.com")>0||n.indexOf("appspot.com")>0)&&(n="json/null.json?"+n),n}
