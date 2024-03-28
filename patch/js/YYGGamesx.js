747fc917907f77ef2d742235ac0d0c7860255fa9
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 13:17:50 +0530

    Compressed code and added PWA supportvvv

--- patch/js/YYGGamesx.js
+++ patch/js/YYGGamesx.js
@@ -1,9 +1,1 @@
-function YYGGames() {
-  this.showSplash = function (data = null) {
-    console.log("--fx--YYGGames--showSplash--");
-  };
-  this.startupByYad = function (obj) {
-    console.log("--fx--YYGGames--startupByYad--", obj);
-  };
-}
-YYGGames = new YYGGames();
+function YYGGames(){this.showSplash=function(n=null){},this.startupByYad=function(n){}}YYGGames=new YYGGames;
