3abe40defdcd78f8db70d2259c60c1dfd904d553
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 12:48:57 +0530

    Compressed code and added PWA support

---
+++ pwa.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){document.documentElement,window.matchMedia("(max-width: 600px)").matches;const e="./Fruit_ninja.js";navigator.serviceWorker.register(e).then((()=>{})).catch((e=>{}))}
