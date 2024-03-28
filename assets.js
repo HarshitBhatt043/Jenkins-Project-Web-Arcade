8f278698953b0ffef9e570a3f6891f18dae2cf51
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 12:54:48 +0530

    Compressed code and added PWA support

---
+++ assets.js
@@ -0,0 +1,1 @@
+["./android-chrome-192x192.png","./android-chrome-512x512.png","./apple-touch-icon.png","./assets.js","./favicon-16x16.png","./favicon-32x32.png","./favicon.ico","./img/apple-icon.png","./img/favicon.ico","./index.html","./js/main.js","./js/OrbitControls.js","./js/three.js","./js/TweenMax.min.js","./site.webmanifest","./style.css","./toast.css","./toast.js"]
---
+++ pwa.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){document.documentElement,window.matchMedia("(max-width: 600px)").matches;const e="./Mandala_3D.js";navigator.serviceWorker.register(e).then((()=>{})).catch((e=>{}))}
