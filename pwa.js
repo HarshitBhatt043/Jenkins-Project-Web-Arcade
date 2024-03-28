d48f9311624f7eb2c4dfe701a3971a1f0168d5e9
Harshit Bhatt <043.harshitbhatt@gmail.com>
2024-03-28 13:15:20 +0530

    Compressed code and added PWA support

---
+++ pwa.js
@@ -0,0 +1,1 @@
+if("serviceWorker"in navigator){document.documentElement,window.matchMedia("(max-width: 600px)").matches;const e="./Skiing_Fred.js";navigator.serviceWorker.register(e).then((()=>{})).catch((e=>{}))}
