f4fee468bdf335697917c71cdc703f51a1219f0b
HarshitBhatt043 <043.harshitbhatt@gmail.com>
2024-08-22 16:47:07 +0530

    Added pwa support

---
+++ assets.js
@@ -0,0 +1,1 @@
+["./","./site.webmanifest","./gzthermal-result.png","./apple-touch-icon.png","./toast.js","./assets.js","./favicon-32x32.png","./__big (1).jpg","./toast.css","./index.html","./android-chrome-512x512.png","./favicon-16x16.png","./favicon.ico","./splash.png","./android-chrome-192x192.png"]
